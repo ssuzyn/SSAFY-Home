05421ec60f3618764822767526063d71a477b815
ssuzyn <asj7090@gmail.com>
2024-11-21 22:29:27 +0900

    add: 회원정보 필드 추가

--- Server/src/main/java/com/ssafy/server/member/dto/MemberInfoDto.java
+++ Server/src/main/java/com/ssafy/server/member/dto/MemberInfoDto.java
@@ -13,16 +13,32 @@ import lombok.ToString;
 @Schema(title = "MemberInfoDto : 회원 정보", description = "회원의 정보를 나타낸다.")
 public class MemberInfoDto {
 
-	@Schema(description = "아이디", example = "snoopy")
+    @Schema(description = "아이디", example = "snoopy")
     private String userId;
+
     @Schema(description = "이름", example = "스누피")
     private String userName;
+
     @Schema(description = "비밀번호", example = "1234")
     private String userPwd;
+
     @Schema(description = "이메일 아이디", example = "snoopy")
     private String emailId;
+
     @Schema(description = "이메일 도메인", example = "gmail.com")
     private String emailDomain;
+
+    @Schema(description = "성별", example = "F")
+    private String gender;
+
+    @Schema(description = "나이", example = "25")
+    private int age;
+
+    @Schema(description = "전화번호", example = "010-1234-5678")
+    private String phoneNumber;
+
     @Schema(description = "프로필 사진 저장 경로")
     private String profileSavePath;
+
+
 }
